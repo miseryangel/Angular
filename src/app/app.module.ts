46c7b4e42c0df25ddd171f9b73d13ed49c4acf5f
miseryangel <miseryangel1990@gmail.com>
2021-10-28 20:44:57 -0400

    spa1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { DishService} from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
 
@@ -43,7 +44,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatButtonModule,
   ],
   providers: [
-    DishService
+    DishService,
+    PromotionService,
   ],
   bootstrap: [AppComponent]
 })
